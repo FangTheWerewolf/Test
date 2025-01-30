b2a5a51ee7b9fc2d2b37f3a4d464100b3b077684
FangTheWerewolf <cfls3264@gmail.com>
2025-01-30 12:41:59 -0500

    Initial Commit

---
+++ Test/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Test
+//
+//  Created by Christopher on 1/30/25.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
